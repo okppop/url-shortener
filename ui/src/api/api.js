4d82da61bef7e8ce4ab1bfb6580ea9ad76488bb5
okppop <okppop@protonmail.com>
2025-02-10 14:31:20 +0800

    feat: api address auto fetch in front end

--- ui/src/api/api.js
+++ ui/src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080"
+const BASE_URL = window.location.origin
 
 export async function CreateURL(data) {
     const response = await fetch(`${BASE_URL}/api/url`, {
